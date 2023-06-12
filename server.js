7ebf1950338ef3ea06ddfe5023ac969735e40ae2
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-06-12 16:56:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path'
+import {fileURLToPath} from 'url'
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-
+//esModule Fix
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename
 //rest object
 const app = express();
 
